2935625bc3f0c679c790aceda55e4566435f13a9
Pannatat Sribusarakham <cenpolarbears@gmail.com>
2024-12-16 23:50:52 +0700

    create CreateTask endpoint

---
+++ routers/tasks.go
@@ -0,0 +1,39 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/cenpnt/Todo-List-BackEnd/initializers"
+	"github.com/cenpnt/Todo-List-BackEnd/models"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateTask(c *gin.Context) {
+	var task models.Task
+
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
+		return
+	}
+	
+	// Hardcoded userID, replace this with JWT later
+	task.UserID = 1
+
+	if task.ParentTaskID != nil {
+		var parentTask models.Task
+		if err := initializers.DB.First(&parentTask, *task.ParentTaskID).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Parent task does not exist"})
+			return
+		}
+	}
+
+	if err := initializers.DB.Create(&task).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create task"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "task created successfully",
+		"task": task,
+	})
+}
