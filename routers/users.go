b4ff7c91fe470a5065150873892a3fb11664553b
Pannatat Sribusarakham <cenpolarbears@gmail.com>
2024-12-17 19:45:42 +0700

    create login endpoint

--- routers/users.go
+++ routers/users.go
@@ -2,10 +2,13 @@ package routers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/cenpnt/Todo-List-BackEnd/initializers"
 	"github.com/cenpnt/Todo-List-BackEnd/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -91,4 +94,41 @@ func GetUserByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, userResponse)
+}
+
+func Login(c *gin.Context) {
+	var body models.User
+
+	if err := c.BindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
+		return
+	}
+
+	var user models.User
+
+	if err := initializers.DB.First(&user, "email = ?", body.Email).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password)); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"token": tokenString,
+	})
 }
