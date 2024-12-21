7045708436eec0fbd44cb9e093df34a834d9bc58
cenpnt <cenpolarbears@gmail.com>
2024-12-21 15:03:31 +0700

    create edit task request

--- models/request.go
+++ models/request.go
@@ -3,3 +3,9 @@ package models
 type ToggleTaskRequest struct {
 	IsCompleted bool `json:"is_completed"`
 }
+
+type EditTaskRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	IsCompleted bool   `json:"is_completed"`
+}
